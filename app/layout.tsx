7cde234ddf5f00096e4d07326884c0e30821686b
Rohan S <113523636+rohhxn@users.noreply.github.com>
2025-12-13 02:57:19 +0530

    Update title font to DM Serif Display with Figma specifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;1,400;1,600&family=Caveat+Brush&display=swap" 
+          href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Caveat+Brush&display=swap" 
           rel="stylesheet" 
         />
       </head>
